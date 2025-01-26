e7aae0fea9e2475931a4841118e1d61a92fa1b2e
Navaneeth <Navaneeth2004pa@gmail.com>
2025-01-26 12:24:51 +0530

    sds

--- app/shop/cart.js
+++ app/shop/cart.js
@@ -230,7 +230,7 @@ export const Cart = ({ navigation }) => {
               onPress={() => navigation.navigate('Shop')}
               style={styles.continueShopping}
             >
-              <Text style={styles.continueShoppingText}>Continue Shop</Text>
+              <Text style={styles.continueShoppingText}>Continue Shopping</Text>
             </TouchableOpacity>
           </View>
         )}
